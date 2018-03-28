82355d67e7251a8afb854f7f65f9d8e227eec165
Ivo Wijnker <ivo@info.nl>
2018-03-28 18:43:38 +0200

    Signature.accepts

--- compiler-core/src/main/kotlin/tinyscript/compiler/core/Entity.kt
+++ compiler-core/src/main/kotlin/tinyscript/compiler/core/Entity.kt
@@ -2,22 +2,35 @@ package tinyscript.compiler.core
 
 import tinyscript.compiler.util.Lazy
 
-sealed class Signature
+sealed class Signature {
+	abstract fun accepts(signature: Signature): Boolean
+}
 
 class NameSignature(
 	val name: String,
 	val isImpure: Boolean,
 	val lazyParamsObjectType: Lazy<ObjectType>?
-) : Signature()
+) : Signature() {
+	override fun accepts(signature: Signature): Boolean =
+		signature is NameSignature &&
+			signature.name == name &&
+			signature.isImpure == isImpure &&
+			if (lazyParamsObjectType != null) {
+				signature.lazyParamsObjectType != null
+					&& lazyParamsObjectType.get().accepts(signature.lazyParamsObjectType.get())
+			} else {
+				signature.lazyParamsObjectType == null
+			}
+}
 
 sealed class Entity
 
 class ValueEntity(
 	val signature: Signature,
 	val lazyType: Lazy<Type>
-): Entity()
+) : Entity()
 
 class TypeEntity(
 	val name: String,
 	val lazyType: Lazy<Type>
-): Entity()
+) : Entity()
--- compiler-core/src/main/kotlin/tinyscript/compiler/core/EntityCollection.kt
+++ compiler-core/src/main/kotlin/tinyscript/compiler/core/EntityCollection.kt
@@ -1,51 +1,33 @@
 package tinyscript.compiler.core
 
 interface EntityCollection {
-	val nameEntities: List<NameEntity>
-	val functionEntities: List<FunctionEntity>
+	val valueEntities: List<ValueEntity>
+
 	val typeEntities: List<TypeEntity>
 
-	fun findNameEntity(name: String, isImpure: Boolean): NameEntity?
-
-	fun findFunctionEntity(
-		name: String, paramsObjectType: ObjectType, isImpure: Boolean
-	): FunctionEntity?
+	fun findValueEntity(signature: Signature): ValueEntity?
 
 	fun findTypeEntity(name: String): TypeEntity?
 }
 
 class MutableEntityCollection : EntityCollection {
-	override val nameEntities: MutableList<NameEntity> = ArrayList()
-	override val functionEntities: MutableList<FunctionEntity> = ArrayList()
+	override val valueEntities: MutableList<ValueEntity> = ArrayList()
+
 	override val typeEntities: MutableList<TypeEntity> = ArrayList()
 
-	override fun findNameEntity(name: String, isImpure: Boolean): NameEntity? =
-		nameEntities.find {
-			it.name == name
-				&& it.isImpure == isImpure
-		}
-
-	override fun findFunctionEntity(
-		name: String, paramsObjectType: ObjectType, isImpure: Boolean
-	): FunctionEntity? =
-		functionEntities.find {
-			it.name == name
-				&& it.isImpure == isImpure
-				&& it.lazyParamsObjectType.get().accepts(paramsObjectType)
-		}
+	override fun findValueEntity(signature: Signature): ValueEntity? =
+		valueEntities.find { it.signature.accepts(signature) }
 
 	override fun findTypeEntity(name: String): TypeEntity? =
 		typeEntities.find { it.name == name }
 }
 
 object EmptyEntityCollection : EntityCollection {
-	override val nameEntities: List<NameEntity> = emptyList()
-	override val functionEntities: List<FunctionEntity> = emptyList()
+	override val valueEntities: List<ValueEntity> = emptyList()
+
 	override val typeEntities: List<TypeEntity> = emptyList()
 
-	override fun findNameEntity(name: String, isImpure: Boolean): NameEntity? = null
-
-	override fun findFunctionEntity(name: String, paramsObjectType: ObjectType, isImpure: Boolean): FunctionEntity? = null
+	override fun findValueEntity(signature: Signature): ValueEntity? = null
 
 	override fun findTypeEntity(name: String): TypeEntity? = null
 }
