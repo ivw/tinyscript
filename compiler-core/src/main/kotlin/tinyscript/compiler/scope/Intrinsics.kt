cf80a995565a681a532cf46645eea746e9312f78
Ivo Wijnker <ivowijnker@hotmail.com>
2018-05-12 17:12:30 +0200

    add intBox to intrinsics

--- compiler-core/src/main/kotlin/tinyscript/compiler/scope/Intrinsics.kt
+++ compiler-core/src/main/kotlin/tinyscript/compiler/scope/Intrinsics.kt
@@ -6,6 +6,29 @@ val floatType = FloatType(Double.MIN_VALUE, Double.MAX_VALUE)
 
 val stringType = AtomicType()
 
+val intBoxType = AtomicType()
+
+val intBoxFunctionSignature = NameSignature(
+	null,
+	"intBox",
+	false,
+	ObjectType(mapOf("value" to intType))
+)
+
+val intBoxSetFunctionSignature = NameSignature(
+	intBoxType,
+	"set",
+	true,
+	ObjectType(mapOf("value" to intType))
+)
+
+val intBoxGetFunctionSignature = NameSignature(
+	intBoxType,
+	"get",
+	true,
+	null
+)
+
 val intTimesIntSignature = OperatorSignature(intType, "*", false, intType)
 
 val intrinsicsScope: Scope = SimpleScope(null)
@@ -13,6 +36,7 @@ val intrinsicsScope: Scope = SimpleScope(null)
 		typeMap["Int"] = intType
 		typeMap["Float"] = floatType
 		typeMap["String"] = stringType
+		typeMap["IntBox"] = intBoxType
 
 		functionMap.add(intTimesIntSignature, { signature ->
 			if (signature is OperatorSignature && signature.lhsType is IntType && signature.rhsType is IntType) {
@@ -22,4 +46,8 @@ val intrinsicsScope: Scope = SimpleScope(null)
 				)
 			} else throw IllegalStateException()
 		})
+
+		functionMap.add(intBoxFunctionSignature, { intBoxType })
+		functionMap.add(intBoxSetFunctionSignature, { AnyType })
+		functionMap.add(intBoxGetFunctionSignature, { intType })
 	}
