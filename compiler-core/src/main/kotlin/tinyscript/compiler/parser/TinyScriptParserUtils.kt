418aae4e4f985f76d5863802c572b480748e7287
Ivo Wijnker <ivowijnker@hotmail.com>
2018-06-28 00:08:01 +0200

    implement custom ANTLRErrorListener

--- compiler-core/src/main/kotlin/tinyscript/compiler/parser/TinyScriptParserUtils.kt
+++ compiler-core/src/main/kotlin/tinyscript/compiler/parser/TinyScriptParserUtils.kt
@@ -1,19 +1,40 @@
 package tinyscript.compiler.parser
 
-import org.antlr.v4.runtime.CharStream
-import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.*
+import org.antlr.v4.runtime.atn.ATNConfigSet
+import org.antlr.v4.runtime.dfa.DFA
+import java.util.*
 
-class ParsingFailedException : RuntimeException(
-	"parsing failed"
-)
+class ParsingFailedException(override val message: String?) : RuntimeException(message)
+
+private class FullANTLRErrorListener : ANTLRErrorListener {
+	override fun reportAttemptingFullContext(recognizer: Parser?, dfa: DFA?, startIndex: Int, stopIndex: Int, conflictingAlts: BitSet?, configs: ATNConfigSet?) {
+		throw ParsingFailedException("attempting full context")
+	}
+
+	override fun syntaxError(recognizer: Recognizer<*, *>?, offendingSymbol: Any?, line: Int, charPositionInLine: Int, msg: String?, e: RecognitionException?) {
+		throw ParsingFailedException("line $line:$charPositionInLine $msg")
+	}
+
+	override fun reportAmbiguity(recognizer: Parser?, dfa: DFA?, startIndex: Int, stopIndex: Int, exact: Boolean, ambigAlts: BitSet?, configs: ATNConfigSet?) {
+		throw ParsingFailedException("ambiguity")
+	}
+
+	override fun reportContextSensitivity(recognizer: Parser?, dfa: DFA?, startIndex: Int, stopIndex: Int, prediction: Int, configs: ATNConfigSet?) {
+		throw ParsingFailedException("context sensitivity")
+	}
+}
 
 fun parseFile(input: CharStream): TinyScriptParser.FileContext {
+	val errorListener = FullANTLRErrorListener()
+
 	val lexer = TinyScriptLexer(input)
+	lexer.removeErrorListeners()
+	lexer.addErrorListener(errorListener)
+
 	val parser = TinyScriptParser(CommonTokenStream(lexer))
-	val fileCtx = parser.file()
+	parser.removeErrorListeners()
+	parser.addErrorListener(errorListener)
 
-	if (parser.numberOfSyntaxErrors > 0)
-		throw ParsingFailedException()
-
-	return fileCtx
+	return parser.file()
 }
